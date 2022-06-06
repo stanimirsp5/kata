7af0b4610c4c03639de04f4bc0225f99b8966e68
Stan <stanimir.sp5@gmail.com>
2022-06-06 20:08:28 +0300

    indexers

--- KataCSharp/Program.cs
+++ KataCSharp/Program.cs
@@ -1,12 +1,7 @@
 ﻿
 using KataCSharp.LeetCode.B;
 
+
 SetMatrixZeroes t = new SetMatrixZeroes();
 t.MyMain();
-return;
-var l1 = new List<int>() { 1,2,3};
-var l2 = new List<int>() { 1,2,3,4};
 
-var isEqual = l1.Equals(l2);
-
-Console.WriteLine(isEqual);
