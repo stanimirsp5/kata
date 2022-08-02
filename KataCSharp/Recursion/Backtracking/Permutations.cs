2d3470e06cd8070b8844c6294ed94513a310358f
stanimir <stanimir.sp5@gmail.com>
2022-08-02 08:58:27 +0300

    permutations

---
+++ KataCSharp/Recursion/Backtracking/Permutations.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace KataCSharp.Recursion.Backtracking
+{
+    public class Permutations
+    {
+        static string letters = "abc";
+        bool[] used = new bool[letters.Length];
+        char[] charArr = new char[letters.Length];
+        List<string> resList = new List<string>();
+        public void Start()
+        {
+            
+            PermuteLetters(0);
+
+        }
+
+        void PermuteLetters(int index)
+        {
+
+            if(index >= letters.Length)
+            {
+                Common.PrintArray(charArr);
+            }
+
+            for (int i = 0; i < letters.Length; i++)
+            {
+                if (!used[i])
+                {
+                    used[i] = true;
+                    charArr[index] = letters[i];
+                    PermuteLetters(index + 1);
+                    used[i] = false;
+                }
+            }
+
+        }
+    }
+}
+
