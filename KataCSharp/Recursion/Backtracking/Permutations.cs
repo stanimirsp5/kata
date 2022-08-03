6ff6b71fe7c53ceef9bdd8de0b9ff419425a92ea
stanimirsp5 <stanimir.sp5@gmail.com>
2022-08-03 10:00:24 +0300

    inplace permutation , with repetition

--- KataCSharp/Recursion/Backtracking/Permutations.cs
+++ KataCSharp/Recursion/Backtracking/Permutations.cs
@@ -3,15 +3,17 @@ namespace KataCSharp.Recursion.Backtracking
 {
     public class Permutations
     {
-        static string letters = "abc";
+        static string letters = "ab";
         bool[] used = new bool[letters.Length];
         char[] charArr = new char[letters.Length];
         List<string> resList = new List<string>();
         public void Start()
         {
             
-            PermuteLetters(0);
-
+           // PermuteLetters(0);
+            //string str = "abc";
+            //InPlacePerm(str, 0, str.Length - 1);
+            PermuteWithRepetition(0);
         }
 
         void PermuteLetters(int index)
@@ -33,6 +35,49 @@ namespace KataCSharp.Recursion.Backtracking
                 }
             }
 
+        }
+
+        void PermuteWithRepetition(int index)
+        {
+
+            if(index >= letters.Length)
+            {
+                Common.PrintArray(charArr);
+                return;
+            }
+
+            for (int s = 0; s < letters.Length; s++)
+            {
+                charArr[index] = letters[s];
+                PermuteWithRepetition(index + 1);
+            }
+
+        }
+
+        void InPlacePerm(string str, int s, int e)
+        {
+
+            if(s >= e)
+            {
+                Console.WriteLine(str);
+                return;
+            }
+
+            for (int i = s; i <= e; i++)
+            {
+                Swap(ref str, s, i);
+                InPlacePerm(str, s + 1, e);
+                Swap(ref str, i, s);
+
+            }
+        }
+        void Swap(ref string str, int s, int e)
+        {
+            char[] charArr = str.ToCharArray();
+            char temp = charArr[s];
+            charArr[s] = charArr[e];
+            charArr[e] = temp;
+            str = new string(charArr);
         }
     }
 }
