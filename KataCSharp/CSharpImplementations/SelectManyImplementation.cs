37eb6f65ceb48e445c9d7d4e9c1ce512df47f240
Stanimir Petrov <stanimir.petrov@egt-digital.com>
2022-10-25 10:05:44 +0300

    select impl

--- KataCSharp/CSharpImplementations/SelectManyImplementation.cs
+++ KataCSharp/CSharpImplementations/SelectManyImplementation.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using AlgorithmsSoftuni.CodingGames.OrderOfSuccession;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 using Xunit;
@@ -16,24 +18,55 @@ namespace KataCSharp.CSharpImplementations
         }
 
 
-        [Fact]
         public void FlattenWithProjectionAndIndex()
         {
             int[] numbers = { 3, 5, 20, 15 };
             var query = numbers.SelectMany((x, index) => (x + index).ToString().ToCharArray(),
                                            (x, c) => x + ": " + c);
             
-           // var query2 = numbers.SelectMany((x, index) => (x + index));
+            var query2 = numbers.SelectMany((x, index) => (index).ToString());
             // 3 => "3: 3"
             // 5 => "5: 6"
             // 20 => "20: 2", "20: 2"
             // 15 => "15: 1", "15: 8"
-            query.SequenceEqual(new List<string> { "3: 3", "5: 6", "20: 2", "20: 2", "15: 1", "15: 8" });
+            //query.SequenceEqual(new List<string> { "3: 3", "5: 6", "20: 2", "20: 2", "15: 1", "15: 8" });
+            //var t =SelectManyImpl(numbers, (x) => x);
+            Func<string, string> selector = str => str.ToUpper();
+            var str = selector("test");
+
+            Console.WriteLine("test2");
+
+        }
+        private static IEnumerable<TResult> SelectImpl<TSource,TResult>(this IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            var t = selector(source.First()); ;
+            return t;
         }
 
+        private static IEnumerable<TResult> SelectManyImpl<TSource, TResult>(IEnumerable<TSource> source, Func<TSource, IEnumerable<TResult>> selector)
+        {
+            Console.WriteLine("test");
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item))
+                {
+                    yield return result;
+                }
+            }
+        }
 
-
-
+        private static IEnumerable<TResult> SelectManyImpl2<TSource, TResult>(
+            IEnumerable<TSource> source,
+            Func<TSource, IEnumerable<TResult>> selector)
+        {
+            foreach (TSource item in source)
+            {
+                foreach (TResult result in selector(item))
+                {
+                    yield return result;
+                }
+            }
+        }
 
     }
 }
