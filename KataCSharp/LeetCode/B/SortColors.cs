e5217ee2619574ebba5013505d95636f79b30075
Stanimir Petrov <stanimir.petrov@egt-digital.com>
2024-06-07 10:28:56 +0300

    Top K Frequent Elements

--- KataCSharp/LeetCode/B/SortColors.cs
+++ KataCSharp/LeetCode/B/SortColors.cs
@@ -14,17 +14,67 @@ namespace KataCSharp.LeetCode.B
         //We will use the integers 0, 1, and 2 to represent the color red, white, and blue, respectively.
         //You must solve this problem without using the library's sort function.
         [Theory]
-        [InlineData(new int[] {2, 0, 2, 1, 1, 0}, new int[] { 0, 0, 1, 1, 2, 2 })]
+        [InlineData(new int[] { 2, 0, 2, 1, 1, 0 }, new int[] { 0, 0, 1, 1, 2, 2 })]
+        [InlineData(new int[] { 7, 5, 1, 10, 8, 9 }, new int[] { 1, 5, 7, 8, 9, 10 })]
         [InlineData(new int[] { 2, 0, 1 }, new int[] { 0, 1, 2 })]
         [InlineData(new int[] { 2, 3, 4, 1 }, new int[] { 1, 2, 3, 4 })]
+        [InlineData(new int[] { 2, 5, 1, 4, 3 }, new int[] { 1, 2, 3, 4, 5 })]
+        [InlineData(new int[] { 2, 5, 1 }, new int[] { 1, 2, 5 })]
         public void SortColorsTest(int[] input, int[] result)
         {
             //var copiedInput = new int[input.Length];
             //Array.Copy(input,copiedInput,input.Length);
-            Sort(input);
+            //InsertionSort(input);
+            QuickSort(input, 0, input.Length-1);
             Assert.Equal(result, input);
 
         }
+
+        // 2, 5, 1 -> 1, 5, 2 -> 1, 2, 5
+        // 2, 5, 1, 4, 3
+        void QuickSort(int[] nums, int low, int high)// 0, 2
+        {
+            if (low < high)
+            {
+
+                int partition = GetPartition(nums, low, high);
+
+                QuickSort(nums, low, partition);
+                QuickSort(nums, partition + 1, high);
+            }
+        }
+
+        private int GetPartition(int[] nums, int low, int high)// 0 ,2
+        {
+            int partition = nums[high];// 1
+            int j = low;// 0
+            for (int i = low; i <= high - 1; i++)
+            {
+                if(nums[i] < partition)// 2<1 f;f
+                {
+                    Swap(nums,i,j);// res 1,5,2
+                    j++;
+                }
+            }
+
+            Swap(nums, j, high);
+
+            return j;
+        }
+
+        private void Swap(int[] nums, int fromNum, int toNum)
+        {
+            int temp = nums[fromNum];
+            nums[fromNum] = nums[toNum];
+            nums[toNum] = temp;
+        }
+
+
+
+
+
+
+
 
         //2, 0, 2, 1, 1, 0
         // 3, 2, 1, 5, 4, 0
@@ -58,6 +108,24 @@ namespace KataCSharp.LeetCode.B
                     nums[n] = currNum;
                 }
             }
+        }
+    
+        void InsertionSort(int[] nums)
+        {
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                int currNum = nums[i];
+                int j = i - 1;
+
+                while (j >= 0 && nums[j] > currNum)
+                {
+                    nums[j + 1] = nums[j];
+                    j--;
+                }
+                nums[j+1] = currNum;
+            }
+
         }
     }
 }
---
+++ KataCSharp/LeetCode/B/TopKFrequentElements.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataCSharp.LeetCode.B
+{
+    public class TopKFrequentElements
+    {
+        //[1,1,1,2,2,3], k = 2
+        public record TestData(int[] nums, int k);
+        [Theory]
+        [MemberData(nameof(GetTestData))]
+        public void TopKFrequentTest(TestData testData, int[] result)
+        {
+            var res = GetTopKFrequent(testData.nums, testData.k);
+
+            Assert.Equal(result, res);
+        }
+        public static IEnumerable<object[]> GetTestData()
+        {
+
+            yield return new object[] { new TestData(new int[] { 2, 4, 1, 3 }, 2), new int[] { 1, 2, 3, 4 } };
+            //yield return new object[] { new TestData(new int[] { 1, 1, 1, 2, 2, 3 }, 2), new int[] { 1, 2 } };
+        }
+
+        //[1,1,1,2,2,3], k = 2
+        // 5, 2, 4, 1, 3
+        public int[] GetTopKFrequent(int[] nums, int k)
+        {
+            Sort(nums, 0, nums.Length);
+            return nums;
+        }
+
+        void Sort(int[] nums, int low, int high)
+        {
+            if (low < high)
+            {
+                int partial = GetPivot(nums, low, high);
+
+                Sort(nums, 0, partial);
+                Sort(nums, partial, high);
+            }
+        }
+
+        int GetPivot(int[] nums, int low, int high)
+        {
+            Console.WriteLine(high - 1);
+            int pivot = nums[high - 1];
+            int j = low;
+            for (int i = 0; i < high; i++)
+            {
+                if (nums[i] < pivot)
+                {
+                    Swap(nums, i, j);
+                    j++;
+                }
+            }
+            Swap(nums, high - 1, j);
+
+            return j;
+        }
+
+        private void Swap(int[] nums, int i, int j)
+        {
+            int temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
