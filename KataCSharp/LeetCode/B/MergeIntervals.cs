26b49c05e9a9cadae72dd3aaf843411041f7b596
Stanimir Petrov <stanimir.petrov@egt-digital.com>
2025-04-17 09:53:31 +0300

    optimize merge with linkedlist and then list

--- KataCSharp/LeetCode/B/MergeIntervals.cs
+++ KataCSharp/LeetCode/B/MergeIntervals.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace KataCSharp.LeetCode.B
 {
-   public class MergeIntervals
-    {
+	public class MergeIntervals
+	{
 
-        [Fact]
-        public void TestMerge()
-        {
+		[Fact]
+		public void TestMerge()
+		{
 			var intervals = new int[][]
 			{
 				new int[] {1, 3},
@@ -30,9 +30,9 @@ namespace KataCSharp.LeetCode.B
 			Assert.Equal(expected.SelectMany(el => el), res.SelectMany(el => el));
 		}
 
-        [Fact]
-        public void TestMerge2()
-        {
+		[Fact]
+		public void TestMerge2()
+		{
 			var intervals = new int[][]
 			{
 				new int[] {1, 4},
@@ -47,9 +47,9 @@ namespace KataCSharp.LeetCode.B
 			Assert.Equal(expected.SelectMany(el => el), res.SelectMany(el => el));
 		}
 
-        [Fact]
-        public void TestMerge3()
-        {
+		[Fact]
+		public void TestMerge3()
+		{
 			var intervals = new int[][]
 			{
 				new int[] {1, 3},
@@ -66,9 +66,9 @@ namespace KataCSharp.LeetCode.B
 			Assert.Equal(expected.SelectMany(el => el), res.SelectMany(el => el));
 		}
 
-        [Fact]
-        public void TestMerge4()
-        {
+		[Fact]
+		public void TestMerge4()
+		{
 			var intervals = new int[][]
 			{
 				new int[] {1, 3}
@@ -82,27 +82,62 @@ namespace KataCSharp.LeetCode.B
 			Assert.Equal(expected.SelectMany(el => el), res.SelectMany(el => el));
 		}
 
+		[Fact]
+		public void TestMerge5()
+		{
+			var intervals = new int[][]
+			{
+				new int[] {1, 4},
+				new int[] {2,3}
+			};
+			var expected = new int[][]
+			{
+				new int[] {1, 4}
+			};
+
+			var res = Merge(intervals);
+			Assert.Equal(expected.SelectMany(el => el), res.SelectMany(el => el));
+		}
+		
+		[Fact]
+		public void TestMerge6()
+		{
+			var intervals = new int[][]
+			{
+				new int[] {1, 4},
+				new int[] {0,2},
+				new int[] {3,5},
+			};
+			var expected = new int[][]
+			{
+				new int[] {0, 5}
+			};
+
+			var res = Merge(intervals);
+			Assert.Equal(expected.SelectMany(el => el), res.SelectMany(el => el));
+		}
+
 
 		// [[1,9],[2,6],[5,10],[2,3]]
 		// [[1,3],[2,4],[5,8]]
-		public int[][] Merge(int[][] intervals)
-        {
+		public int[][] Merge2(int[][] intervals)
+		{
 			// order by first element
 			intervals = intervals.OrderBy(el => el[0]).ToArray();
 			//check if second element on curr arr is bigger than first on next arr
 			var numOne = intervals[0][0];
 			var numTwo = intervals[0][1];
-			var res = new List<int[]>(); 
-			
+			var res = new List<int[]>();
+
 			if (intervals.Length == 1)
 				res.Add(new int[] { numOne, numTwo });
 
 			for (int i = 1; i < intervals.Length; i++)
 			{
 				var arr = intervals[i];
-				if(numTwo >= arr[0])
+				if (numTwo >= arr[0])
 				{
-					if(numTwo < arr[1])
+					if (numTwo < arr[1])
 					{
 						numTwo = arr[1];
 					}
@@ -116,12 +151,54 @@ namespace KataCSharp.LeetCode.B
 					numOne = arr[0];
 					numTwo = arr[1];
 
-					if(i == intervals.Length-1)
+					if (i == intervals.Length - 1)
 						res.Add(new int[] { numOne, numTwo });
 				}
 
 			}
 			return res.ToArray();
+		}
+
+		// Optimize with LinkedLists
+		public int[][] Merge3(int[][] intervals)
+		{
+			Array.Sort(intervals, (a,b) => a[0] - b[0]);
+			var linkedList = new LinkedList<int[]>();
+
+			foreach (var interval in intervals)
+			{
+				if (linkedList.Count == 0 || linkedList.Last.Value[1] < interval[0])
+				{
+					linkedList.AddLast(interval);
+				}
+				else
+				{
+					linkedList.Last.Value[1] = Math.Max(interval[1], linkedList.Last.Value[1]);
+
+				}
+			}
+
+			return linkedList.ToArray();
+		}
+
+		//With Lists
+		public int[][] Merge(int[][] intervals)
+		{
+			Array.Sort(intervals, (a, b) => a[0] - b[0]);
+			var list = new List<int[]>();
+			for (int i = 0; i < intervals.Length; i++)
+			{
+				var n = list.Count;
+				if (list.Count == 0 || list[n - 1][1] < intervals[i][0])
+				{
+					list.Add(intervals[i]);
+				}
+				else
+				{
+					list[n - 1][1] = Math.Max(intervals[i][1], list[n - 1][1]);
+				}
+			}
+			return list.ToArray();
 		}
 
 		////https://stackoverflow.com/questions/45937278/how-to-test-a-method-parameter-type-jagged-array-with-xunit
@@ -136,5 +213,5 @@ namespace KataCSharp.LeetCode.B
 		//}
 
 
-    }
+	}
 }
