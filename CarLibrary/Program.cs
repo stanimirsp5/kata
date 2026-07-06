99595dfcae86a407e0e95cc5b511dd9c589bd83e
Stanimir Petrov <stanimir.petrov@egt-digital.com>
2026-07-06 17:11:29 +0300

    edit .il files

--- CarLibrary/Program.cs
+++ CarLibrary/Program.cs
@@ -1,1 +1,33 @@
 ﻿Console.WriteLine("Hello, World!");
+
+var car = new CarLibrary.Car
+{
+	Brand = "Toyota",
+	Model = "Camry",
+	Year = 2022
+};
+
+var activateBoost = false;
+
+Console.WriteLine("Do you want to activate turbo boost? (yes/no)");
+var input = Console.ReadLine();
+if (input?.ToLower() == "yes" || input?.ToLower() == "y")
+{
+	activateBoost = true;
+}
+
+if(activateBoost)
+{
+	car.TurboBoost();
+}
+else
+{
+	Console.WriteLine("Turbo boost not activated.");
+}
+Console.ReadLine();
+
+// Create .il file in the CarLibrary folder:
+// ildasm /all /METADATA /out=.\CarLibrary\CarLibrary.il .\CarLibrary\bin\Debug\net10.0\CarLibrary.dll
+//Compile .il file to .dll file in the CarLibrary folder:
+// ilasm /DLL CarLibrary.il /x64
+// ilasm /dll /output=.\CarLibrary\CarLibrary.dll .\CarLibrary\CarLibrary.il
