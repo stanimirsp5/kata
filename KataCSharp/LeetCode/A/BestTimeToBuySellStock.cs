fe3c83dab60474873400941f2b411d7afb523836
Stanimir Petrov <stanimir.petrov@egt-digital.com>
2026-07-21 11:21:26 +0300

    MaximumSubarraySum

--- KataCSharp/LeetCode/A/BestTimeToBuySellStock.cs
+++ KataCSharp/LeetCode/A/BestTimeToBuySellStock.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using KataCSharp.LeetCode.B;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace KataCSharp.LeetCode.A
 {
@@ -37,17 +40,17 @@ namespace KataCSharp.LeetCode.A
 		[InlineData(new int[] { 27, 2, 5, 3, 6, 4, 1, 3 }, 4)]
 		[InlineData(new int[] { 2, 1, 2, 1, 0, 1, 2 }, 2)]
 		[InlineData(new int[] { 3, 3, 5, 0, 0, 3, 1, 4 }, 4)]
-		public void Start(int[] prices, int expectedResult)
+		public void MaxProfit(int[] prices, int expectedResult)
 		{
-			var res = MaxProfit(prices);
+			var res = FindMaxProfit(prices);
 
 			Assert.Equal(expectedResult, res);
 		}
 
-		
-		public int MaxProfit(int[] prices)
+
+		public int FindMaxProfit(int[] prices)
 		{
-			int[] minPrice = { -1 ,int.MaxValue };
+			int[] minPrice = { -1, int.MaxValue };
 			int[] maxPrice = { -1, int.MinValue };
 			var profit = -1;
 
@@ -79,5 +82,29 @@ namespace KataCSharp.LeetCode.A
 			return profit;
 		}
 
+
+		// Maximum Subarray Sum - Kadane's Algorithm
+		//https://www.geeksforgeeks.org/dsa/largest-sum-contiguous-subarray/ 
+
+		//Given an integer array arr[], find the subarray(containing at least one element) which has the maximum possible sum, and return that sum.
+		//Note: A subarray is a continuous part of an array.
+
+		//Examples:
+
+		//Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+		//Output: 11
+		//Explanation: The subarray [7, -1, 2, 3] has the largest sum 11.
+
+		//Input: arr[] = [-2, -4]
+		//Output: -2
+		//Explanation: The subarray [-2] has the largest sum -2.
+
+		//Input: arr[] = [5, 4, 1, 7, 8]
+		//Output: 25
+		//Explanation: The subarray [5, 4, 1, 7, 8] has the largest sum 25.
+		public int MaximumSubarraySum()
+		{
+			return 0;
+		}
 	}
 }
